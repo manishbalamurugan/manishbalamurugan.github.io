9ba086946d6ac4edefedda4a3e27e68898ea9216
Manish Balamurugan <manishbalamurugan@gmail.com>
2023-06-19 00:18:18 -0400

    [FIX] MDX use.

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,6 @@ import remarkGfm from 'remark-gfm'
 import rehypePrism from '@mapbox/rehype-prism'
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ['jsx', 'mdx'],
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    newNextLinkBehavior: true,
-    scrollRestoration: true,
-  },
-}
-
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
   options: {
@@ -21,8 +11,15 @@ const withMDX = nextMDX({
   },
 })
 
-module.exports = withMDX({
+const nextConfig = {
+  pageExtensions: ['jsx', 'mdx'],
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    newNextLinkBehavior: true,
+    scrollRestoration: true,
+  },
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-})
+}
 
 export default withMDX(nextConfig)
