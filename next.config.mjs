35e0f97ba965d05bfa68b5525c38f6034af169b9
Manish Balamurugan <manishbalamurugan@gmail.com>
2023-06-19 02:11:37 -0400

    [UPDATE] Web 1.1

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,24 @@
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+import rehypePrism from '@mapbox/rehype-prism'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['jsx', 'mdx'],
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
     newNextLinkBehavior: true,
     scrollRestoration: true,
-    images: {
-      unoptimized: true,
-    },
   },
-  pageExtensions: ['js', 'jsx'],
-  basePath: '/manishbalamurugan.github.io',
-  assetPrefix: '/manishbalamurugan.github.io/',
 }
 
-export default nextConfig
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
+  },
+})
+
+export default withMDX(nextConfig)
